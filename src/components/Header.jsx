eb4518d14dffb8cba9f3d7463281d922a25919ac
HarshitAe12 <harshit@authorityentrepreneurs.com>
2025-10-07 16:38:09 +0530

    updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
         <header className="bg-white px-6 py-4 flex justify-between items-center ">
             {/* Left: Home Button */}
 
-            <img src={logo} alt="Logo" className="h-10 w-[150px] object-contain" />
+            <img src="https://images.leadconnectorhq.com/image/f_webp/q_80/r_1200/u_https://assets.cdn.filesafe.space/8gxGtPEVqtv0pHte3WSh/media/68da47adf00445478b6d27e4.png" alt="Logo" className="h-10 w-[150px] object-contain" />
             {/* Right: Logo */}
             <button onClick={()=>navigate("/")} className="bg-[#1A2C40] cursor-pointer text-white font-semibold px-4 py-2 rounded hover:bg-blue-700 transition">
                 Home
